3327748feae67438ac66fca764de9bbe2d248f0c
StellaPaischi <131032816+StellaPaischi@users.noreply.github.com>
2023-07-01 20:23:21 +0200

    Update Motivation.py

--- Motivation.py
+++ Motivation.py
@@ -1,7 +1,35 @@
 import streamlit as st
-#Bild: st.image()
-#st.write("vv") : text
-#st.header("vv")
+def main():
+    st.header("Moin und auch hier nochmal Herzlich Willkommen zu VoiceChoice!")
+
+    st.write("Es handelt es sich um eine App, die in der Lage ist, männliche und weibliche Stimmen zu erkennen.")
+    st.write("Doch bevor wir zu dieser App gekommen sind, haben wir intensiv über andere Ideen nachgedacht.")
+    
+             
+    st.subheader("Welche Ideen hatten wir?")
+    
+    st.subheader("1. Analyse von Essen")
+    st.write("Eine Überlegung war die Analyse von Essen. Die App sollte anhand von Fotos von Mahlzeiten Informationen über Nährwerte, Kaloriengehalt und ähnliche Daten liefern kann. Diese Funktion könnte beispielsweise in Restaurants oder der Lebensmittelindustrie von großem Nutzen sein, um Kunden detaillierte Informationen über ihre Mahlzeiten zu bieten.")
+    st.write("Wieso haben wir das nicht genommen?")
+    st.write("Es wäre schwierig, genaue Informationen über Nährwerte und Kaloriengehalt ohne tiefgreifendes Wissen im Bereich der Nahrungsmittelanalyse zu liefern.")
+  
+    st.subheader("2. Erkennung von Tiergeräuschen")
+    st.write("Eine weitere Idee war die Fähigkeit zur Erkennung von Tiergeräuschen. Dies hätte insbesondere in landwirtschaftlichen oder zoologischen Anwendungen von großem Vorteil sein können, um Tierarten anhand ihrer charakteristischen Geräusche zu identifizieren und Überwachungssysteme zu verbessern.")
+    st.write("Wieso haben wir das nicht genommen?")
+    st.write("Die Vielfalt der Tierstimmen und die Komplexität der Umgebungsgeräusche hätten es schwierig gemacht, eine genaue Klassifizierung zu erreichen.")
+    
+    st.subheader("3. Musikgenre-Erkennung")
+    st.write("Diese Funktion könnte Musikliebhabern dabei helfen, neue Künstler und Songs zu entdecken, die ihrem individuellen Geschmack entsprechen.")
+    st.write("Wieso haben wir das nicht genommen?")
+    st.write("Die Vielfalt der musikalischen Stile und die individuellen Unterschiede in den Stimmen machten eine präzise Klassifizierung schwierig.")
+   
+    st.subheader("4. Nutzung von Schall zur Bestimmung von Entfernungen.")
+    st.write("Dies wäre eine App mit der man abschätzen könnte, wie weit entfernt eine Person oder ein Gegenstand ist. Dies könnte in Situationen, in denen die visuelle Wahrnehmung eingeschränkt ist, äußerst nützlich sein.")
+
+    st.header("Aber jetzt zu VoiceChoice")
+    st.write("
+
+  
 #st.subheader("vv")
 #st.markdown()
 #st.title()
