ce68ed760739bfd733c0bde7bded605fe8499c8d
yunico27 <132067831+yunico27@users.noreply.github.com>
2023-07-02 23:48:31 +0200

    Update Dokumentation.py

--- Dokumentation.py
+++ Dokumentation.py
@@ -7,8 +7,15 @@ def main():
         initial_sidebar_state = "collapsed"
     )
 
+
+
+
+
+
 st.title("Dokumentation unserer App VoiceChoice")
-st.write("Die VoiceChoice-App ist eine Anwendung, die entwickelt wurde, um anhand einer Audiodatei das Geschlecht der Stimme zu erkennen. Die App verwendet eine Kombination von Algorithmen und Machine Learning-Techniken, um eine Aussage darüber zu treffen, ob die aufgenommene Stimme weiblich oder männlich ist. Diese Dokumentation gibt einen Überblick über die Funktionen der App.")
+st.write("Die VoiceChoice-App ist eine Anwendung, die entwickelt wurde, um anhand einer Audiodatei das Geschlecht der Stimme zu erkennen. Die App verwendet eine Kombination von Algorithmen und Machine Learning-Techniken, um eine Aussage darüber zu treffen, ob die aufgenommene Stimme weiblich oder männlich ist. Diese Dokumentation gibt einen kurzen Überblick über die Funktionen der App.")
+
+
 
 
 if __name__ == "__main__":
