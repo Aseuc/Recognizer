1fb05babf9468e19773c4a241ac5f577c430f531
StellaPaischi <131032816+StellaPaischi@users.noreply.github.com>
2023-07-02 14:44:40 +0200

    Update Motivation.py

--- Motivation.py
+++ Motivation.py
@@ -117,10 +117,10 @@ def main():
         st.write("Unser großes Ziel ist die Erkennung von einzelnen Personen und Emotionen. Um den Alltag zu erleichern und Sicherheit anzustreben. ")
         st.write("Überlegt haben wir uns Verfahren wie Sprachbiometrie zur Personenerkennung und Algorithmen des maschinellen Lernens zur Emotionserkennung zu nutzen. Durch kontinuierliches Training mit mehr und umfangreicheren Daten und Validierung in realen Anwendungen wollen wir die Genauigkeit verbessern und eine personalisierte und reaktionsschnelle Benutzererfahrung anstreben.")
 
-    st.subheader("Zusätzliche Animation:")
-        additional_animation = load_lottie_animation(additional_lottie_url)
-        if additional_animation:
-            st_lottie(additional_animation, width=200, height=200, key="additional_animation")
-    
+        st.subheader("Zusätzliche Animation:")
+            additional_animation = load_lottie_animation(additional_lottie_url)
+            if additional_animation:
+                st_lottie(additional_animation, width=200, height=200, key="additional_animation")
+        
 if __name__ == "__main__":
     main()
