c3daa4573254369a84950c9a09954cd46a41f779
StellaPaischi <131032816+StellaPaischi@users.noreply.github.com>
2023-07-02 11:01:27 +0200

    Update Motivation.py
    neu: Buttons

--- Motivation.py
+++ Motivation.py
@@ -2,29 +2,32 @@ import streamlit as st
 def main():
     st.header("Moin und auch hier nochmal Herzlich Willkommen zu VoiceChoice!")
 
-    st.write("Es handelt es sich um eine App, die in der Lage ist, männliche und weibliche Stimmen zu erkennen.")
+    st.write("Bei VoiceChoice handelt es sich um eine App, die in der Lage ist, männliche und weibliche Stimmen zu erkennen.")
     st.write("Doch bevor wir zu dieser App gekommen sind, haben wir intensiv über andere Ideen nachgedacht.")
-    
-             
+        
     st.subheader("Welche Ideen hatten wir?")
-    
-    st.subheader("1. Analyse von Essen")
-    st.write("Eine Überlegung war die Analyse von Essen. Die App sollte anhand von Fotos von Mahlzeiten Informationen über Nährwerte, Kaloriengehalt und ähnliche Daten liefern kann. Diese Funktion könnte beispielsweise in Restaurants oder der Lebensmittelindustrie von großem Nutzen sein, um Kunden detaillierte Informationen über ihre Mahlzeiten zu bieten.")
-    st.write("Wieso haben wir das nicht genommen?")
-    st.write("Es wäre schwierig, genaue Informationen über Nährwerte und Kaloriengehalt ohne tiefgreifendes Wissen im Bereich der Nahrungsmittelanalyse zu liefern.")
-  
-    st.subheader("2. Erkennung von Tiergeräuschen")
-    st.write("Eine weitere Idee war die Fähigkeit zur Erkennung von Tiergeräuschen. Dies hätte insbesondere in landwirtschaftlichen oder zoologischen Anwendungen von großem Vorteil sein können, um Tierarten anhand ihrer charakteristischen Geräusche zu identifizieren und Überwachungssysteme zu verbessern.")
-    st.write("Wieso haben wir das nicht genommen?")
-    st.write("Die Vielfalt der Tierstimmen und die Komplexität der Umgebungsgeräusche hätten es schwierig gemacht, eine genaue Klassifizierung zu erreichen.")
-    
-    st.subheader("3. Musikgenre-Erkennung")
-    st.write("Diese Funktion könnte Musikliebhabern dabei helfen, neue Künstler und Songs zu entdecken, die ihrem individuellen Geschmack entsprechen.")
-    st.write("Wieso haben wir das nicht genommen?")
-    st.write("Die Vielfalt der musikalischen Stile und die individuellen Unterschiede in den Stimmen machten eine präzise Klassifizierung schwierig.")
-   
-    st.subheader("4. Nutzung von Schall zur Bestimmung von Entfernungen.")
-    st.write("Dies wäre eine App mit der man abschätzen könnte, wie weit entfernt eine Person oder ein Gegenstand ist. Dies könnte in Situationen, in denen die visuelle Wahrnehmung eingeschränkt ist, äußerst nützlich sein.")
+
+    # Button 1 - Analyse von Essen
+    if st.button("1. Analyse von Essen"):
+        st.write("Eine Überlegung war die Analyse von Essen. Die App sollte anhand von Fotos von Mahlzeiten Informationen über Nährwerte, Kaloriengehalt und ähnliche Daten liefern kann. Diese Funktion könnte beispielsweise in Restaurants oder der Lebensmittelindustrie von großem Nutzen sein, um Kunden detaillierte Informationen über ihre Mahlzeiten zu bieten.")
+        st.write("Wieso haben wir das nicht genommen?")
+        st.write("Es wäre schwierig, genaue Informationen über Nährwerte und Kaloriengehalt ohne tiefgreifendes Wissen im Bereich der Nahrungsmittelanalyse zu liefern.")
+
+    # Button 2 - Erkennung von Tiergeräuschen
+    if st.button("2. Erkennung von Tiergeräuschen"):
+        st.write("Eine weitere Idee war die Fähigkeit zur Erkennung von Tiergeräuschen. Dies hätte insbesondere in landwirtschaftlichen oder zoologischen Anwendungen von großem Vorteil sein können, um Tierarten anhand ihrer charakteristischen Geräusche zu identifizieren und Überwachungssysteme zu verbessern.")
+        st.write("Wieso haben wir das nicht genommen?")
+        st.write("Die Vielfalt der Tierstimmen und die Komplexität der Umgebungsgeräusche hätten es schwierig gemacht, eine genaue Klassifizierung zu erreichen.")
+
+    # Button 3 - Musikgenre-Erkennung
+    if st.button("3. Musikgenre-Erkennung"):
+        st.write("Diese Funktion könnte Musikliebhabern dabei helfen, neue Künstler und Songs zu entdecken, die ihrem individuellen Geschmack entsprechen.")
+        st.write("Wieso haben wir das nicht genommen?")
+        st.write("Die Vielfalt der musikalischen Stile und die individuellen Unterschiede in den Stimmen machten eine präzise Klassifizierung schwierig.")
+
+    # Button 4 - Nutzung von Schall zur Bestimmung von Entfernungen
+    if st.button("4. Nutzung von Schall zur Bestimmung von Entfernungen"):
+        st.write("Dies wäre eine App mit der man abschätzen könnte, wie weit entfernt eine Person oder ein Gegenstand ist. Dies könnte in Situationen, in denen die visuelle Wahrnehmung eingeschränkt ist, äußerst nützlich sein.")
 
     st.header("Aber jetzt zu VoiceChoice")
     st.write("Inspiriert von Filmen wie James Bond, in denen der Held mit seiner Stimme Türen und Safes öffnet, haben wir uns entschlossen, eine Stimmerkennungs-App zu entwickeln, die nicht nur beeindruckend ist, sondern auch viele weitere Vorteile bietet.")
@@ -50,6 +53,4 @@ def main():
     st.write("Unser großes Ziel ist die Erkennung von einzelnen Personen und Emotionen. Um den Alltag zu erleichern und Sicherheit anzustrebe. ")
     st.write(" Überlegt haben wir uns Verfahren wie Sprachbiometrie zur Personenerkennung und Algorithmen des maschinellen Lernens zur Emotionserkennung zu nutzen. Durch kontinuierliches Training mit mehr und umfangreicheren Daten und Validierung in realen Anwendungen wollen wir die Genauigkeit verbessern und eine personalisierte und reaktionsschnelle Benutzererfahrung anstreben.")
   
-#st.subheader("vv")
-#st.markdown()
-#st.title()
+
