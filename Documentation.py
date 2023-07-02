e504a9006b0c6ea6e59d15f6c77c4f3a5cef0c09
yunico27 <132067831+yunico27@users.noreply.github.com>
2023-07-02 21:51:46 +0200

    Update Documentation.py

--- Documentation.py
+++ Documentation.py
@@ -1,7 +1,15 @@
 import streamlit as st
-#Bild: st.image()
-#st.write("vv") : text
-#st.header("vv")
-#st.subheader("vv")
-#st.markdown()
-#st.title()
+
+def main():
+    st.set_page_config(
+        page_title="Documentation",
+        layout='wide',
+        initial_sidebar_state = "collapsed"
+    )
+
+st.title("Documentation unserer App VoiceChoice")
+    st.write("Die VoiceChoice-App ist eine Anwendung, die entwickelt wurde, um anhand einer Audiodatei das Geschlecht der Stimme zu erkennen. Die App verwendet eine Kombination von Algorithmen und Machine Learning-Techniken, um eine Aussage darüber zu treffen, ob die aufgenommene Stimme weiblich oder männlich ist. Diese Dokumentation gibt einen Überblick über die Funktionen der App.")
+
+
+if __name__ == "__main__":
+    main()
