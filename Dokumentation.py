a82918b53282881cf09ec86f60a720ba88a0156b
yunico27 <132067831+yunico27@users.noreply.github.com>
2023-07-02 23:25:34 +0200

    Update and rename Documentation.py to Dokumentation.py

---
+++ Dokumentation.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+def main():
+    st.set_page_config(
+        page_title="Documentation",
+        layout='wide',
+        initial_sidebar_state = "collapsed"
+    )
+
+st.title("Dokumentation unserer App VoiceChoice")
+
+st.write("Die VoiceChoice-App ist eine Anwendung, die entwickelt wurde, um anhand einer Audiodatei das Geschlecht der Stimme zu erkennen. Die App verwendet eine Kombination von Algorithmen und Machine Learning-Techniken, um eine Aussage darüber zu treffen, ob die aufgenommene Stimme weiblich oder männlich ist. Diese Dokumentation gibt einen Überblick über die Funktionen der App.")
+
+
+if __name__ == "__main__":
+    main()
