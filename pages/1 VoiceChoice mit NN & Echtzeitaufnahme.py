5a6342949d778a75ee69edbc8d9f305bef384f55
Aseuc <busse1997@googlemail.com>
2023-07-02 22:21:52 +0200

    Update

--- pages/1 VoiceChoice mit NN & Echtzeitaufnahme.py
+++ pages/1 VoiceChoice mit NN & Echtzeitaufnahme.py
@@ -1,13 +1,34 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, ClientSettings
-import soundfile as sf
+from aiortc.contrib.media import MediaRecorder
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, ClientSettings, WebRtcMode
+from pydub import AudioSegment
 import os
 import numpy as np
+import VoiceChoice as vc
+import pandas as pd
+from datetime import datetime, timedelta
+
+
+def shuffle_excel_rows(filename, sheet_name=0):
+    # Read the Excel file into a DataFrame
+    df = pd.read_excel(filename, sheet_name=sheet_name)
+
+    # Shuffle the rows of the DataFrame
+    df = df.sample(frac=1).reset_index(drop=True)
+
+    # Write the shuffled DataFrame back to the Excel file
+    df.to_excel(filename, sheet_name=sheet_name, index=False)
 
 
 def record_audio():
-    st.set_page_config(page_title="Audio Recorder", page_icon=":microphone:")
-    st.title("Audio Recorder")
+    try:
+        st.set_page_config(page_title="VoiceChoice NN & Echtzeitaufnahme", page_icon="favicon.ico", layout="wide")
+    except Exception as e:
+        print(e)
+    st.title('VoiceChoice mit NN & "Echzeitaufnahme"')
+
+    def recorder_factory():
+        return MediaRecorder("tempDir2/record.wav")
 
     class AudioRecorder(VideoTransformerBase):
         def __init__(self):
@@ -17,23 +38,43 @@ def record_audio():
             self.audio_frames.append(frame.to_ndarray())
             return frame
 
-    webrtc_ctx = webrtc_streamer(key="audio-recorder", video_processor_factory=AudioRecorder,
-                                 audio_receiver_size=256,
-                                 client_settings=ClientSettings(
-                                     rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
-                                     media_stream_constraints={"video": False, "audio": True},
-                                 ))
-    recorder = webrtc_ctx.video_processor
+    webrtc_ctx = webrtc_streamer(
+        key="sendonly-audio",
+        mode=WebRtcMode.SENDONLY,
+        in_recorder_factory=recorder_factory,
+        client_settings=ClientSettings(
+            rtc_configuration={
+                "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+            },
+            media_stream_constraints={
+                "audio": True,
+                "video": False,
+            },
+        ),
+    )
+
+    recorder = webrtc_ctx.audio_processor
+
+    start_time = datetime.now()
+    timer_text = st.empty()
+
     if webrtc_ctx.state.playing:
-        if len(recorder.audio_frames) > 0:
-            filename = os.path.join("C:/Users/busse/Neuer Ordner (6)/Recognizer/tempDir2", "my_recording.wav")
-            audio_data = np.hstack(recorder.audio_frames)
-            sf.write(filename, audio_data, 48000)
-            st.audio(filename)
+        current_time = datetime.now()
+        elapsed_time = current_time - start_time
+        timer_text.text(f"Elapsed time: {str(elapsed_time)[:-7]}")
+
+    if st.button("Neuronales Netz starten"):
+        excel_file = vc.get_single_excel_with_features_no_label("tempDir2/record.wav", "tempDir2/", 10, True)
+        vc.neuronal_network("TDNN3.xlsx", excel_file, 4, [32, 32, 32, 32])
+        for file in os.listdir("tempDir2/"):
+            if file.endswith(".xlsx"):
+                os.remove(f"tempDir2/{file}")
+            if file.endswith(".wav"):
+                os.remove(f"tempDir2/{file}")
 
 
 if __name__ == "__main__":
     try:
         record_audio()
     except Exception as e:
-            print(e)
+        print(e)
--- pages/1 VoiceChoice mit Random Forest Classifier.py
+++ pages/1 VoiceChoice mit Random Forest Classifier.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 from sklearn.metrics import accuracy_score
 from pydub import AudioSegment
@@ -9,30 +8,32 @@ from sklearn.ensemble import RandomForestClassifier
 from datetime import datetime
 import VoiceChoice as vc
 import ballons_blue as bb
-import ballons_red  as br
+import ballons_red as br
 import randomFacts as rf
-
 
 
 def mp4_to_wav(mp4_file, wav_file):
     audio = AudioSegment.from_file(mp4_file, format="mp4")
     audio.export(f"tempDir/{wav_file}", format="wav")
 
+
 def get_current_date_time():
     now = datetime.now()
-    date_time_str =now.strftime("%d_%m_%Y_%H_%M_%S_%f_%Z")
+    date_time_str = now.strftime("%d_%m_%Y_%H_%M_%S_%f_%Z")
     return date_time_str + "_"
 
+
 def upload_and_convert():
-    uploaded_file = st.file_uploader("Wählen Sie eine Datei zum Hochladen aus", type=["mp4", "wav"], key="file_uploader3")
+    uploaded_file = st.file_uploader("Wählen Sie eine Datei zum Hochladen aus", type=["mp4", "wav"],
+                                     key="file_uploader3")
     print(uploaded_file)
     if uploaded_file is not None:
-        file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type}
+        file_details = {"FileName": uploaded_file.name, "FileType": uploaded_file.type}
         st.write(file_details)
-        with open(os.path.join("tempDir2",uploaded_file.name),"wb") as f:
+        with open(os.path.join("tempDir2", uploaded_file.name), "wb") as f:
             f.write(uploaded_file.getbuffer())
         if uploaded_file.type == "video/mp4":
-            mp4_to_wav(uploaded_file,get_current_date_time() + ".wav")
+            mp4_to_wav(uploaded_file, get_current_date_time() + ".wav")
             return True
         else:
             st.success("Hochgeladene Datei ist bereits im WAV-Format")
@@ -52,15 +53,16 @@ def load_data(excel_file):
     df = pd.read_excel(excel_file)
     return df
 
+
 try:
-    if upload_and_convert() == True: 
+    if upload_and_convert() == True:
         for file in os.listdir("tempDir2/"):
             if file.endswith(".wav"):
-                excel_File = vc.get_single_excel_with_features_no_label(f"tempDir2/{file}",f"tempDir2/",1,False)
+                excel_File = vc.get_single_excel_with_features_no_label(f"tempDir2/{file}", f"tempDir2/", 1, False)
                 # st.write(excel_File)
-                
+
                 df = load_data("TrainDataRFohneID1.xlsx")
-                X = df.drop(["label"],axis=1)
+                X = df.drop(["label"], axis=1)
                 y = df["label"]
                 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
                 rfc = RandomForestClassifier(n_estimators=100, random_state=42)
@@ -69,22 +71,20 @@ try:
                 accuracy = accuracy_score(y_test, y_pred)
                 # with open('ML_Models/RandomForestClassifier.pkl', 'wb') as file:
                 #      pickle.dump(rfc, file)
-                
-                
-                st.write(f"Die Genauigkeit des Random Forest Klassifikators auf den Testdatensatz beträgt: {accuracy:.2f}")   
-                st.write("Hier nochmal genauer die Predictions der Testdatensätze 0 = Mann, 1 = Frau:" , y_pred)
+
+                st.write(
+                    f"Die Genauigkeit des Random Forest Klassifikators auf den Testdatensatz beträgt: {accuracy:.2f}")
+                st.write("Hier nochmal genauer die Predictions der Testdatensätze 0 = Mann, 1 = Frau:", y_pred)
 
                 # with open('ML_Models/RandomForestClassifier.pkl', 'rb') as file:
                 #         rfc = pickle.load(file)
                 data = pd.read_excel(excel_File)
-                data = data.drop(["Unnamed: 0"], axis=1)            
+                data = data.drop(["Unnamed: 0"], axis=1)
                 st.write(data)
                 data = data.drop(["label"], axis=1)
                 st.write(data)
                 y_pred2 = rfc.predict(data)
                 st.write(y_pred2)
-
-                
 
                 # df = load_data("TrainDataRFohneID1.xlsx")
                 # X = df.drop(["label"], axis=1)
@@ -100,45 +100,41 @@ try:
                 # grid_search = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=5)
 
                 # grid_search.fit(X_train, y_train)
-                
+
                 # st.write(f"Die besten Hyperparameter sind: {grid_search.best_params_}")
-                
+
                 # best_model = grid_search.best_estimator_
-                
+
                 # y_pred = best_model.predict(X_test)
-                
+
                 # accuracy = accuracy_score(y_test, y_pred)
-                
+
                 # st.write(f"Die Genauigkeit des Random Forest Klassifikators auf den Testdatensatz beträgt: {accuracy:.2f}")
                 # st.write("Hier nochmal genauer die Predictions der Testdatensätze 0 = Mann, 1 = Fraue:", y_pred)
 
-                if y_pred2 == 0: 
+                if y_pred2 == 0:
                     bb.ballons_blue()
-                    st.markdown("<h3 style='text-align: center;'>Die Person auf der Aufnahme scheint ein Mann zu sein!</h3>", unsafe_allow_html=True)
+                    st.markdown(
+                        "<h3 style='text-align: center;'>Die Person auf der Aufnahme scheint ein Mann zu sein!</h3>",
+                        unsafe_allow_html=True)
                     st.markdown("<h3 style='text-align: center;'>Wusstest du schon?</h3>", unsafe_allow_html=True)
                     st.markdown(f"<h4 style='text-align: center;'>{rf.random_fact_men()}</h4>", unsafe_allow_html=True)
                 elif y_pred2 == 1:
                     br.ballons_red()
-                    st.markdown("<h3 style='text-align: center;'>Die Person auf der Aufnahme scheint eine Frau zu sein!</h3>", unsafe_allow_html=True)
+                    st.markdown(
+                        "<h3 style='text-align: center;'>Die Person auf der Aufnahme scheint eine Frau zu sein!</h3>",
+                        unsafe_allow_html=True)
                     st.markdown("<h3 style='text-align: center;'>Wusstest du schon?</h3>", unsafe_allow_html=True)
-                    st.markdown(f"<h4 style='text-align: center;'>{rf.random_fact_women()}</h4>", unsafe_allow_html=True)
+                    st.markdown(f"<h4 style='text-align: center;'>{rf.random_fact_women()}</h4>",
+                                unsafe_allow_html=True)
 
-
-
-                st.balloons()    
+                st.balloons()
                 for file in os.listdir("tempDir2/"):
-                            if file.endswith(".wav"):    
-                                        os.remove(os.path.join("tempDir2", file))
-                            if file.endswith(".xlsx"):
-                                    os.remove(os.path.join("tempDir2",(file)))
-                            if file.endswith(".mp4"):
-                                    os.remove(os.path.join("tempDir2",(file)))
+                    if file.endswith(".wav"):
+                        os.remove(os.path.join("tempDir2", file))
+                    if file.endswith(".xlsx"):
+                        os.remove(os.path.join("tempDir2", (file)))
+                    if file.endswith(".mp4"):
+                        os.remove(os.path.join("tempDir2", (file)))
 except Exception as e:
-        print(e)
-
-
-
-
-
-
-
+    print(e)
